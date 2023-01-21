962a52f8306a43c3b9042e0ff43dd2fb1ceea8e0
Danielle Valcarcel <dvalcarcel72@gmail.com>
2023-01-21 11:25:02 -0500

    new token

--- commands/hey.js
+++ commands/hey.js
@@ -7,21 +7,27 @@ module.exports = {
         if (message.author.id == 203261206969057280) { // Danielle
             replies = [
                 "Hello Mother uwu",
-                "Make way for smelly supreme!"
+                "Make way for smelly supreme!",
+                "Hello! You look fabulous today!",
+                "*Hee hee ha hoo hee ha hoooo*",
+                "watch out for a bunch of boneless women flopping through the streets"
             ];
         } else if (message.author.id == 640046013729472512) { // Fionna
             replies = [
                 "https://www.youtube.com/watch?v=2N6AcMv2OGE",
                 "Here comes the clownmeister herself~",
                 "HONK HONK :clown:",
-                "Ew. It's youuuuu..."
+                "Ew. It's youuuuu...",
+                "The group villainess? With a title like that you're bound to go \"mwa ha ha \" at some point, I just know it...",
             ];
         } else if (message.author.id == 270276933894275074) { // Janet
             replies = [
                 "Hewwo! OwO!!",
                 "pee pee poo poo",
                 "Hi Janet! Still simping, I see...",
-                "Haiiii! UwU"
+                "Haiiii! UwU",
+                "KACHOW!!!",
+                "What would I do without my favourite simp? I'd cry myself to sleep."
             ];
         } else if (message.author.id == 616315469774716934) { // Angela
             replies = [
@@ -29,14 +35,17 @@ module.exports = {
                 "こんにちは",
                 "大丈夫ですか",
                 "誰があんたを惑星に置いたのか",
-                "The weeb cometh."
+                "The weeb cometh.",
+                "How's that MHA video essay coming along?"
             ];
         } else if (message.author.id == 235835476655734785) { // Melody
             replies = [
                 "Hi! Don't forget the sauce!",
                 "Hiya! Please tell me you didn't hurt yourself today <3",
                 "Hey Melody! Any funny work stories?",
-                "Hiii! You're looking fabulous today!"
+                "Hiii! You're looking fabulous today!",
+                "Your collar bones are looking SEXY today~",
+                "Greetings Professor! Nothing to report!"
             ];
         } else if (message.author.id == 432020359164723200) { // Yvette
             replies = [
--- commands/magic_conch.js
+++ commands/magic_conch.js
@@ -33,11 +33,28 @@ module.exports = {
             "Very doubtful tbh.",
             "Sounds like an iss-YOU not an iss-ME",
             "You're really gonna ask me that?",
-            "Try asking again."
+            "Try asking again.",
+            "Nah.",
+            "yuh",
+            "How about... no.",
+            "Did you think about that question before you asked?",
+            "The Powers That Be say so",
+            "You don't want to know. Trust me.",
+            "How am I supposed to know lmao",
+            "\"+conch\" this, \"+conch\" that... Why does everything fall to me?"
         ];
 
-        var i = Math.floor(Math.random() * answers.length);
-        var answer = answers[i];
+        // answers.length + 1 = Bugs Bunny No
+        // answers.length + 2 = elmo shrug
+
+        var i = Math.floor(Math.random() * (answers.length + 2));
+        var answer;
+        if (i <= answers.length){
+            answer = answers[i];
+        } else {
+            answer = "";
+        }
+        
 
         const embed = new Discord.MessageEmbed()
         .setColor('#9B83A2')
@@ -46,9 +63,20 @@ module.exports = {
         .setImage('https://static.wikia.nocookie.net/spongebob/images/9/9b/Club_SpongeBob_151.png/revision/latest?cb=20200208100215');
 
         const reply = new Discord.MessageEmbed()
-        .setColor('#9B83A2')
-        .setTitle("\"" + answer + "\"")
-        .setImage('https://i.pinimg.com/originals/c1/3d/0b/c13d0bdd3ab710d7285629f284a43d4d.png');
+            .setColor('#9B83A2');
+        
+        if (i <= answers.length) {
+            reply.setTitle("\"" + answer + "\"");
+        }
+            
+
+        if (i <= answers.length) {
+            reply.setImage('https://i.pinimg.com/originals/c1/3d/0b/c13d0bdd3ab710d7285629f284a43d4d.png');
+        } else if (i == answers.length + 1) {
+            reply.setImage('https://i.kym-cdn.com/photos/images/newsfeed/001/483/348/bdd.jpg'); 
+        } else if (i == answers.length + 2) {
+            reply.setImage('https://media4.giphy.com/media/1VUkQ2k7tGYOLWA1Mo/source.gif');
+        }
 
         let filter = m => m.author.id === message.author.id;
         
